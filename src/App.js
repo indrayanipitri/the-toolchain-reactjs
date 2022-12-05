acd50d5589262d2e2ff44714bc948e1e07912c19
Pitri Indrayani <indrayanipitri@gmail.com>
2022-12-06 03:38:09 +0700

    membuat component di file terpisah

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { WelcomeH3 } from './Welcome';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WelcomeH3 children="Welcome H3"/>
+      <WelcomeH3 children="Selamat Datang H3"/>
+      <WelcomeH3> Aku adalah content </WelcomeH3>
     </div>
   );
 }
---
+++ src/Welcome.js
@@ -0,0 +1,11 @@
+// export default function Welcome() {
+//     return <h1>Welcome</h1>
+// }
+
+// export function WelcomeH2() {
+//     return <h2>Welcome H2</h2>
+// }
+
+export function WelcomeH3(props) {
+    return <h3>{props.children}</h3>
+}
